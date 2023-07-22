6a11ba802a7cbf808877751ecf9f950c7db577f4
Rizki Januar I <104696196+rzkjanuarr@users.noreply.github.com>
2023-07-22 07:44:36 +0700

    final chapter

---
+++ components/Layout/Footer.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Logo from "../../public/assets/Logo.svg";
+import Facebook from "../../public/assets/Icon/facebook.svg";
+import Twitter from "../../public/assets/Icon/twitter.svg";
+import Instagram from "../../public/assets/Icon/instagram.svg";
+const Footer = () => {
+  return (
+    <div className="bg-white-300 pt-44 pb-24">
+      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
+        <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
+          <Logo className="h-8 w-auto mb-6" />
+          <p className="mb-4">
+            <strong className="font-medium">Unesa Learn</strong> adalah
+            pembelajaran online untuk milenial kaum muda-mudi Unesa.
+          </p>
+          <div className="flex w-full mt-2 mb-8 -mx-2">
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Facebook className="h-6 w-6" />
+            </div>
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Twitter className="h-6 w-6" />
+            </div>
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Instagram className="h-6 w-6" />
+            </div>
+          </div>
+          <p className="text-gray-400">
+            ©{new Date().getFullYear()} - Unesa Learn!
+          </p>
+        </div>
+        <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Produk</p>
+          <ul className="text-black-500 ">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Download{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Pricelist{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Lokasi{" "}
+            </li>
+          </ul>
+        </div>
+        <div className="row-span-2 sm:col-span-2 sm:col-start-9 sm:col-end-11 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Keuntungan</p>
+          <ul className="text-black-500">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Unesa Learn{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              FAQ{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Tutorial{" "}
+            </li>
+          </ul>
+        </div>
+        <div className="row-span-2 sm:col-span-2 sm:col-start-11 sm:col-end-13 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Afiliasi</p>
+          <ul className="text-black-500">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Mau duit?{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Gabung bersama kami!{" "}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/SeoHead.js
@@ -0,0 +1,105 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+// Default value for some meta data
+const defaultMeta = {
+  title: "UnesaLearn",
+  siteName: "UnesaLearn",
+  description:
+    "Unesa Learn!",
+  // change base url of your web (without '/' at the end)
+  url: "https://unesa-learn.vercel.app",
+  type: "website",
+  robots: "follow, index",
+  // change with url of your image (recommended dimension = 1.91:1)
+  // used in twitter, facebook, etc. card when link copied in tweet/status
+  image: "https://unesa-learn.vercel.app/assets/card-image.png",
+  author: "Lorem Ipsum",
+};
+
+/**
+ * Next Head component populated with necessary SEO tags and title
+ * props field used:
+ * - title
+ * - siteName
+ * - description
+ * - url
+ * - type
+ * - robots
+ * - image
+ * - date
+ * - author
+ * - templateTitle
+ * all field are optional (default value defined on defaultMeta)
+ * @example
+ * <SeoHead title="Page's Title" />
+ */
+const SeoHead = (props) => {
+  const router = useRouter();
+  const meta = {
+    ...defaultMeta,
+    ...props,
+  };
+
+  // Use siteName if there is templateTitle
+  // but show full title if there is none
+  meta.title = props.templateTitle
+    ? `${props.templateTitle} | ${meta.siteName}`
+    : meta.title;
+
+  return (
+    <Head>
+      <title>{meta.title}</title>
+      <meta name="robots" content={meta.robots} />
+      <meta content={meta.description} name="description" />
+      <meta property="og:url" content={`${meta.url}${router.asPath}`} />
+      <link rel="canonical" href={`${meta.url}${router.asPath}`} />
+      {/* Open Graph */}
+      <meta property="og:type" content={meta.type} />
+      <meta property="og:site_name" content={meta.siteName} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:title" content={meta.title} />
+      <meta name="image" property="og:image" content={meta.image} />
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@F2aldi" />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
+      <meta name="twitter:image" content={meta.image} />
+      {meta.date && (
+        <>
+          <meta property="article:published_time" content={meta.date} />
+          <meta
+            name="publish_date"
+            property="og:publish_date"
+            content={meta.date}
+          />
+          <meta name="author" property="article:author" content={meta.author} />
+        </>
+      )}
+      {/* Favicons */}
+      {favicons.map((linkProps) => (
+        <link key={linkProps.href} {...linkProps} />
+      ))}
+      {/* Windows 8 app icon */}
+      <meta name="msapplication-TileColor" content="#1B2143" />
+      <meta
+        name="msapplication-TileImage"
+        content="/favicon/ms-icon-144x144.png"
+      />
+      {/* Accent color on supported browser */}
+      <meta name="theme-color" content="#1B2143" />
+    </Head>
+  );
+};
+
+// Favicons, other icons, and manifest definition
+const favicons = [
+  {
+    rel: "unesa-learn",
+    sizes: "60x60",
+    href: "/favicon/U+.png",
+  },
+];
+
+export default SeoHead;
